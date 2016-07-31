3b7b2582d64054419404ce30ce46198bce6ce986
wertrain <wertrain@gmail.com>
2016-07-31 18:00:10 +0900

    fix indent

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,23 @@ var gulp = require('gulp'),
     jasmine = require('gulp-jasmine');
 
 gulp.task('webserver', function() {
-  gulp.src('app')
-    .pipe(webserver({
-      livereload: true,
-      open: true
-    }));
+    gulp.src('app')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
 });
 
 gulp.task('test', function() {
-  gulp.src('spec/test.js')
-    .pipe(jasmine());
+    gulp.src('spec/test.js')
+        .pipe(jasmine());
 });
 
 gulp.task('eslint', function() {
-  return gulp.src(['app/script/*.js']) // lint のチェック先を指定
-    .pipe(eslint({ useEslintrc: true })) // .eslintrc を参照
-    .pipe(eslint.format())
-    .pipe(eslint.failOnError());
+    return gulp.src(['app/script/*.js'])
+        .pipe(eslint({ useEslintrc: true }))
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
 });
 
 gulp.task('default', ['webserver']);
