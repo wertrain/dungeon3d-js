3c64a4b4d603e7f566cea623ccdb2a82245dba79
wertrain <wertrain@gmail.com>
2016-08-01 00:12:47 +0900

    add test

--- spec/map_spec.js
+++ spec/map_spec.js
@@ -2,7 +2,19 @@
 
 var exports = require('../app/script/map.js');
 
-describe('test case name', function() {
+describe('Array2d', function() {
+    beforeEach(function() {
+        this.array2d = new exports.Array2d(50, 40);
+        this.array2d[1][0] = 64;
+    });
+    it('test', function () {
+        expect(this.array2d[1][0]).toBe(64);
+        expect(this.array2d.getWidth()).toBe(50);
+        expect(this.array2d.getHeight()).toBe(40);
+    });
+});
+
+describe('Map', function() {
     beforeEach(function() {
         this.map = new exports.Map();
         this.map.initalize();
