aef30143dd4d207885238535147014769bdd141c
wertrain <wertrain@gmail.com>
2016-08-26 01:22:27 +0900

    add chara file

---
+++ app/script/character.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+    /** キャラクター */
+    let CharaData = function() {
+        this.x = 0; // 2D マップ上のX位置
+        this.y = 0; // 2D マップ上のY位置
+        this.direction = 0; // 方向
+        this.pose = 0; // 姿勢
+        this.position = []; // 3D 上の位置
+    };
+}
