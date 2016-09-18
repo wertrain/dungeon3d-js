0d20bbebcadb86b770dd6401858039b40ce2dea8
wertrain <wertrain@gmail.com>
2016-09-18 13:22:14 +0900

    add test

---
+++ spec/math_spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var exports = require('../app/script/math.js');
+
+describe('Vector3', function() {
+    beforeEach(function() {
+        this.vector = exports.Vector3.create();
+    });
+    it('test', function () {
+        
+    });
+});
+
+describe('Matrix44', function() {
+    beforeEach(function() {
+        this.matrix = exports.Matrix44.create();
+    });
+    it('test', function () {
+
+    });
+});
